78be1fdb02ca33851f5a5a31150c810f3c54364e
Atrufulgium <atrufulgium@gmail.com>
2024-05-05 13:00:16 +0200

    Return extraction
    * Rewriter that, together with arg extraction, ensures the actual signature of the method is no longer relevant.

---
+++ BulletScriptCompiler/Compiler/Visitors/ExtractReturnRewriter.cs
@@ -0,0 +1,140 @@
+﻿using Atrufulgium.BulletScript.Compiler.Semantics;
+using Atrufulgium.BulletScript.Compiler.Syntax;
+
+namespace Atrufulgium.BulletScript.Compiler.Visitors {
+    /// <summary>
+    /// This class performs the following rewrites:
+    /// <list type="bullet">
+    /// <item>
+    /// Method definitions
+    /// <code>
+    ///     type method( .. ) {
+    ///         ..
+    ///     }
+    /// </code>
+    /// turn into
+    /// <code>
+    ///     type method#return;
+    ///     void method( .. ) {
+    ///         ..
+    ///     }
+    /// </code>
+    /// </item>
+    /// <item>
+    /// Non-empty return statements
+    /// <code>
+    ///     return expression;
+    /// </code>
+    /// (re)turn into
+    /// <code>
+    ///     method#return = expression;
+    ///     return;
+    /// </code>
+    /// </item>
+    /// <item>
+    /// Method calls whose returns are used
+    /// <code>
+    ///     identifier = method( .. );
+    ///     // and similar
+    /// </code>
+    /// turn into
+    /// <code>
+    ///     method();
+    ///     identifier = method#return;
+    ///     // and similar
+    /// </code>
+    /// <br/>
+    /// Exception: intrinsics are unaffected.
+    /// </item>
+    /// </list>
+    /// </summary>
+    /// <remarks>
+    /// Assumptions:
+    /// <list type="bullet">
+    /// <item><b>ASSUMPTIONS BEFORE:</b> ExtractMethodArgsRewriter has run, and all branched return. </item>
+    /// <item><b>ASSUMPTIONS AFTER:</b> Idem, and all methods are void-typed. </item>
+    /// </list>
+    /// </remarks>
+    // TODO: I don't check the "all branches return" _anywhere_ yet.
+    internal class ExtractReturnRewriter : AbstractTreeRewriter {
+
+        static IdentifierName ReturnName(MethodSymbol symbol) => new($"{symbol.FullyQualifiedName}#return");
+
+        // What to rename each parameter to when putting it outside the method.
+        IdentifierName currentReturnName = new("");
+        protected override Node? VisitMethodDeclaration(MethodDeclaration node) {
+            var symbol = Model.GetSymbolInfo(node);
+            currentReturnName = ReturnName(symbol);
+
+            // Don't do anything when void.
+            if (symbol.Type == Syntax.Type.Void)
+                return base.VisitMethodDeclaration(node);
+            
+            node = (MethodDeclaration)base.VisitMethodDeclaration(node)!;
+            return new MultipleDeclarations(
+                new VariableDeclaration(currentReturnName, symbol.Type),
+                node.WithType(Syntax.Type.Void)
+            );
+        }
+
+        protected override Node? VisitReturnStatement(ReturnStatement node) {
+            node = (ReturnStatement)base.VisitReturnStatement(node)!;
+            if (node.ReturnValue == null)
+                return node;
+            return new MultipleStatements(
+                new ExpressionStatement(
+                    new AssignmentExpression(
+                        currentReturnName,
+                        AssignmentOp.Set,
+                        node.ReturnValue
+                    )
+                ),
+                new ReturnStatement()
+            );
+        }
+
+        // The logic here is entirely analogous to the logic in
+        /// <see cref="ExtractMethodArgsRewriter"/>
+        // so see those comments.
+        bool insideInvocation = false;
+        readonly List<Statement> prependedStatements = new();
+
+        protected override Node? VisitStatement(Statement node) {
+            prependedStatements.Clear();
+            node = (Statement)base.VisitStatement(node)!;
+
+            if (prependedStatements.Count == 0)
+                return node;
+            return new MultipleStatements(
+                new MultipleStatements(prependedStatements),
+                node
+            );
+        }
+
+        protected override Node? VisitInvocationExpression(InvocationExpression node) {
+            if (insideInvocation)
+                throw new VisitorAssumptionFailedException("Assumed no invocation is contained in another.");
+
+            var methodSymbol = Model.GetSymbolInfo(node);
+
+            insideInvocation = true;
+            node = (InvocationExpression)base.VisitInvocationExpression(node)!;
+            insideInvocation = false;
+
+            // Intrinsics don't need to do anything.
+            if (methodSymbol.IsIntrinsic)
+                return node;
+
+            prependedStatements.Add(
+                new ExpressionStatement(node)
+            );
+            return ReturnName(methodSymbol);
+        }
+
+        protected override Node? VisitRoot(Root node) {
+            if (node.Declarations.Count == 0)
+                return node;
+            return base.VisitRoot(node);
+        }
+    }
+}
---
+++ BulletScriptCompiler/CompilerTests/Visitors/ExtractReturnRewriterTests.cs
@@ -0,0 +1,216 @@
+﻿using Atrufulgium.BulletScript.Compiler.Tests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atrufulgium.BulletScript.Compiler.Visitors.Tests {
+
+    [TestClass]
+    public class ExtractReturnRewriterTests {
+
+        [TestMethod]
+        public void TestRegular() => TestHelpers.AssertGeneratesTree(@"
+function float my_float() { return 3; }
+function void A() { float a = my_float(); }
+", @"
+[root]
+declarations:
+    [variable declaration]
+    identifier:
+        [identifier name]
+        name:
+            my_float()#return
+    type:
+        float
+    initializer:
+        [none]
+    [method declaration]
+    identifier:
+        [identifier name]
+        name:
+            my_float
+    type:
+        void
+    arguments:
+        [none]
+    block:
+        [block]
+        statements:
+            [expression statement]
+            statement:
+                [assignment]
+                lhs:
+                    [identifier name]
+                    name:
+                        my_float()#return
+                op:
+                    =
+                rhs:
+                    [literal float]
+                    value:
+                        3
+            [return]
+            value:
+                [none]
+    [method declaration]
+    identifier:
+        [identifier name]
+        name:
+            A
+    type:
+        void
+    arguments:
+        [none]
+    block:
+        [block]
+        statements:
+            [expression statement]
+            statement:
+                [invocation]
+                target:
+                    [identifier name]
+                    name:
+                        my_float
+                args:
+                    [none]
+            [local declaration]
+            declaration:
+                [variable declaration]
+                identifier:
+                    [identifier name]
+                    name:
+                        a
+                type:
+                    float
+                initializer:
+                    [identifier name]
+                    name:
+                        my_float()#return
+", new ExtractReturnRewriter());
+
+        [TestMethod]
+        public void TestArithmetic() => TestHelpers.AssertGeneratesTree(@"
+function float my_float() { return 3; }
+function void A() { float a = 1 + my_float(); }
+", @"
+[root]
+declarations:
+    [variable declaration]
+    identifier:
+        [identifier name]
+        name:
+            my_float()#return
+    type:
+        float
+    initializer:
+        [none]
+    [method declaration]
+    identifier:
+        [identifier name]
+        name:
+            my_float
+    type:
+        void
+    arguments:
+        [none]
+    block:
+        [block]
+        statements:
+            [expression statement]
+            statement:
+                [assignment]
+                lhs:
+                    [identifier name]
+                    name:
+                        my_float()#return
+                op:
+                    =
+                rhs:
+                    [literal float]
+                    value:
+                        3
+            [return]
+            value:
+                [none]
+    [method declaration]
+    identifier:
+        [identifier name]
+        name:
+            A
+    type:
+        void
+    arguments:
+        [none]
+    block:
+        [block]
+        statements:
+            [expression statement]
+            statement:
+                [invocation]
+                target:
+                    [identifier name]
+                    name:
+                        my_float
+                args:
+                    [none]
+            [local declaration]
+            declaration:
+                [variable declaration]
+                identifier:
+                    [identifier name]
+                    name:
+                        a
+                type:
+                    float
+                initializer:
+                    [binop]
+                    lhs:
+                        [literal float]
+                        value:
+                            1
+                    op:
+                        +
+                    rhs:
+                        [identifier name]
+                        name:
+                            my_float()#return
+", new ExtractReturnRewriter());
+
+        [TestMethod]
+        public void TestIntrinsic() => TestHelpers.AssertGeneratesTree(@"
+function void A() { float a = sin(230); }
+", @"
+[root]
+declarations:
+    [method declaration]
+    identifier:
+        [identifier name]
+        name:
+            A
+    type:
+        void
+    arguments:
+        [none]
+    block:
+        [block]
+        statements:
+            [local declaration]
+            declaration:
+                [variable declaration]
+                identifier:
+                    [identifier name]
+                    name:
+                        a
+                type:
+                    float
+                initializer:
+                    [invocation]
+                    target:
+                        [identifier name]
+                        name:
+                            sin
+                    args:
+                        [literal float]
+                        value:
+                            230
+", new ExtractReturnRewriter());
+    }
+}
