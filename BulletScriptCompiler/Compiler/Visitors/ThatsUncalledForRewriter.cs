ea8b3c11cde50178fda2bcacc84b10dbb98109c0
Atrufulgium <atrufulgium@gmail.com>
2024-05-09 11:58:22 +0200

    Removing uncalled methods
    the lamest pun
    * A rewriter that removes methods that aren't called (not taking into account what branches are actually reachable).

---
+++ BulletScriptCompiler/Compiler/Visitors/ThatsUncalledForRewriter.cs
@@ -0,0 +1,68 @@
+﻿using Atrufulgium.BulletScript.Compiler.Syntax;
+
+namespace Atrufulgium.BulletScript.Compiler.Visitors {
+    /// <summary>
+    /// This class performs the following rewrite:
+    /// <list type="bullet">
+    /// <item>
+    /// Uncalled methods (transitively)
+    /// <code>
+    ///     function A() { B(); }
+    ///     function B() { }
+    /// </code>
+    /// get removed.
+    /// <br/>
+    /// The exception are the methods the VM calls directly, such as <c>main()</c>.
+    /// </item>
+    /// </list>
+    /// </summary>
+    /// <remarks>
+    /// Assumptions:
+    /// <list type="bullet">
+    /// <item><b>ASSUMPTIONS BEFORE:</b> None. </item>
+    /// <item><b>ASSUMPTIONS AFTER:</b> None. </item>
+    /// </list>
+    /// </remarks>
+    internal class ThatsUncalledForRewriter : AbstractTreeRewriter {
+
+        protected override Node? VisitRoot(Root node) {
+            if (node.RootLevelStatements.Count > 0)
+                return node;
+
+            // Do passes until nothing gets removed.
+            var semanticModel = Model;
+            while (true) {
+                IVisitor rewriter = new RemovalPassRewriter() { Model = semanticModel };
+                rewriter.Visit(node);
+                if (!((RemovalPassRewriter)rewriter).RemovedAnything)
+                    break;
+                node = (Root)rewriter.VisitResult!;
+                semanticModel = new(node);
+            }
+            return node;
+        }
+
+
+        private class RemovalPassRewriter : AbstractTreeRewriter {
+
+            public bool RemovedAnything { get; private set; } = false;
+
+            protected override Node? VisitMethodDeclaration(MethodDeclaration node) {
+                var symbol = Model.GetSymbolInfo(node);
+                // MethodSymbol.CalledBy only references bulletscript-internal
+                // calls. The VM calls are disregarded, so that is an extra check.
+                if (symbol.IsSpecialMethod)
+                    return node;
+                if (symbol.CalledBy.Count == 0) {
+                    RemovedAnything = true;
+                    return null;
+                }
+                return node;
+            }
+
+            // Other stuff doesn't need to be handled so quick return w/o children
+            protected override Node? VisitVariableDeclaration(VariableDeclaration node)
+                => node;
+        }
+    }
+}
---
+++ BulletScriptCompiler/CompilerTests/Visitors/ThatsUncalledForRewriterTests.cs
@@ -0,0 +1,26 @@
+﻿using Atrufulgium.BulletScript.Compiler.Tests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atrufulgium.BulletScript.Compiler.Visitors.Tests {
+
+    [TestClass]
+    public class ThatsUncalledForRewriterTests {
+
+        [TestMethod]
+        public void Test() => TestHelpers.AssertGeneratesTree(@"
+function void A() { B(); }
+function void B() { C(); }
+function void C() { D(); }
+function void D() { }
+function void ActuallyCalled() { }
+function void main(float value) { ActuallyCalled(); }
+", @"
+[root]
+    [method declaration]     void ActuallyCalled ()
+            [none]
+    [method declaration]     void main (float value)
+            [expression]             ActuallyCalled()
+", compactTree: true, new ThatsUncalledForRewriter());
+
+    }
+}
